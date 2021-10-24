611553b5169543171124741c8f300903c8913e0d
MaksymilianK <kondziugeniusz@gmail.com>
2021-10-24 13:17:09 +0200

    Init project

---
+++ src/main/java/com/github/maksymiliank/arrivalrulethemallproxy/ArrivalRuleThemAllProxy.java
@@ -0,0 +1,11 @@
+package com.github.maksymiliank.arrivalrulethemallproxy;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class ArrivalRuleThemAllProxy extends Plugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+}
